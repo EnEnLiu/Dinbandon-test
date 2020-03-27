27c4b5cb5e5c2005614f068fdda14a6b40308987
LiuEnTzu <tainn_50554@yahoo.com.tw>
2020-03-27 22:49:39 +0800

    update

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get '/' , to: "welcome#index"
+
+  get '/login' , to: "users#login"
+
+  get '/sign_up' , to: "users#sign_up"
+
+  post '/sign_up' , to: "users#registration"
+ 
+end
