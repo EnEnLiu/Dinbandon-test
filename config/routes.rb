e315a0a9bf9c13e92cd6133ba1463b4107a65f1e
LiuEnTzu <tainn_50554@yahoo.com.tw>
2020-04-01 15:21:30 +0800

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get '/' , to: "welcome#index"
-
+  #root "welcome#index"
   get '/login' , to: "users#login"
+
+  delete '/logout', to: "users#logout"
 
   get '/sign_up' , to: "users#sign_up"
 
