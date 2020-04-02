417e1b3a3bab19e9fdb843e5a3ded88a49248d2e
LiuEnTzu <tainn_50554@yahoo.com.tw>
2020-04-02 23:49:11 +0800

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-  #get '/' , to: "welcome#index"
+  #根目錄get '/' , to: "welcome#index"
   root "welcome#index"
 
+
+  #users
   get '/login' , to: "users#login"
   post '/login', to: "users#sign_in"
   delete '/logout', to: "users#logout"
-
   get '/sign_up' , to: "users#sign_up"
   post '/sign_up' , to: "users#registration"
+
+  #items
+  resources :items
  
 end
