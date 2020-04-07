9feaf09f4a9fe6525b8b0b2314103b7d0ecac0eb
LiuEnTzu <tainn_50554@yahoo.com.tw>
2020-04-07 12:18:19 +0800

    update event

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
   validates :password, presence: true, 
                        confirmation: true,
                        length: { minimum: 4 }
+
+  has_many :histories
+  has_many :events, through: :histories
 end
